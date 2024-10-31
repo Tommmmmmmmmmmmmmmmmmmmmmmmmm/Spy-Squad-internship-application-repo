543dd37339cc42e433a94c48e881b32c172c1d9a
Tommmmmmmmmmmmmmmmmmmmmmmmmm <to316505@falmouth.ac.uk>
2024-10-31 17:52:43 +0000

    Fixed hierarchy

---
+++ Most complex script or small collection of scripts/FloatSpringHandler.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using NaughtyAttributes;
+using System;
+
+public class FloatSpringHandler : ArbitraryValueSpringHandler<FloatSpring, float>
+{
+    protected override bool IShouldPrintDebugStatements { get { return false; } }
+    protected override float ValueToTarget { get { return myValue; }  set { myValue = value; } }
+    protected override bool IShouldContinueSpringing => Mathf.Abs(myValue - AToBSpring.EndValue) > .005f || !AToBSpring.IsSpringVelocityNearZero();
+
+    protected override void CalculateOutput(FloatSpring mySpring)
+    {
+        if (output == null) return;
+
+        OnSpringUpdate?.Invoke(myValue);
+        float distance = Mathf.Abs(ValueToTarget - mySpring.EndValue);
+        OutputData(distance);
+    }
+}
